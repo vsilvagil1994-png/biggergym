e2a2cd4da5967cf7fea3240ba1c8c673e7c7853d
Victoria Silva <vsilvagil1994@gmail.com>
2026-01-19 18:12:25 -0500

    Config conexión MySQL Railway

--- backend/db.js
+++ backend/db.js
@@ -2,13 +2,15 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
 });
 
 module.exports = pool.promise();
+
