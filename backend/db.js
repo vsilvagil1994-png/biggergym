7974a9ae6470d7ace72d0624c75cf14cc440cbca
Victoria Silva <vsilvagil1994@gmail.com>
2026-01-20 17:51:33 -0500

    Fix conexión MySQL Railway

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,14 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
-const pool = mysql.createPool(process.env.MYSQL_PUBLIC_URL);
+const pool = mysql.createPool({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
 
-module.exports = pool.promise();
-
-
-
+module.exports = pool;
